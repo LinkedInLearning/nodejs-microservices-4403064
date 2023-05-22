ae182c78b258dc836c9b5b9f6d17764fae5953ab
Daniel Khan <daniel@khan.io>
2023-05-22 23:45:16 +0200

    correct end state

--- workspace/microservices/catalog-service/routes/index.js
+++ workspace/microservices/catalog-service/routes/index.js
@@ -1,74 +1,11 @@
 const express = require("express");
 
-const CatalogService = require("../lib/CatalogService");
-
 const router = express.Router();
 
-function returnJson(item) {
-  return { id: item._id, name: item.name, sku: item.sku, price: item.price };
-}
-
-router.get("/items", async (req, res) => {
-  try {
-    const items = await CatalogService.getAll();
-    return res.json(items.map(returnJson));
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("General error");
-  }
-});
-
-router.get("/items/:id", async (req, res) => {
-  try {
-    const item = await CatalogService.getOne(req.params.id);
-
-    if (!item) {
-      return res.status(404).send("Item not found");
-    }
-
-    return res.json(returnJson(item));
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("General error");
-  }
-});
-
-router.post("/items", async (req, res) => {
-  try {
-    const item = await CatalogService.create(req.body);
-    return res.json(returnJson(item));
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("General error");
-  }
-});
-
-router.put("/items/:id", async (req, res) => {
-  try {
-    const item = await CatalogService.update(req.params.id, req.body);
-    if (!item) {
-      return res.status(404).send("Item not found");
-    }
-    return res.json(returnJson(item));
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("General error");
-  }
-});
-
-router.delete("/items/:id", async (req, res) => {
-  try {
-    const result = await CatalogService.remove(req.params.id);
-
-    if (!result.deletedCount === 0) {
-      return res.status(404).send("Item not found");
-    }
-
-    return res.status(204);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("General error");
-  }
+// Define your RESTful routes here
+router.get("/", (req, res) => {
+  // Return a JSON response with a 'hello world' message
+  res.json({ msg: "hello world" });
 });
 
 module.exports = router;
