bda147ea8a880c4ef52816a60ef9e9b3677a9bd0
Daniel Khan <daniel@khan.io>
2023-05-23 21:30:02 +0200

    Video endstate

--- workspace/microservices/catalog-service/routes/index.js
+++ workspace/microservices/catalog-service/routes/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 
+const CatalogService = require("../lib/CatalogService");
+
 const router = express.Router();
 
-// Define your RESTful routes here
-router.get("/", (req, res) => {
-  // Return a JSON response with a 'hello world' message
-  res.json({ msg: "hello world" });
+router.get("/items", async (req, res) => {
+  try {
+    const items = await CatalogService.getAll();
+    return res.json(items);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 module.exports = router;
