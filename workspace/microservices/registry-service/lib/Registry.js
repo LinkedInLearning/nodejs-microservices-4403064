86f19c8fd657c226ce9a981abc4611bbd4620e1d
Daniel Khan <daniel@khan.io>
2023-05-26 12:21:11 +0200

    video finish

--- workspace/microservices/registry-service/lib/Registry.js
+++ workspace/microservices/registry-service/lib/Registry.js
@@ -4,8 +4,13 @@ class Registry {
     this.timeout = 15;
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  getKey(name, version, ip, port) {
+    return name + version + ip + port;
+  }
+
   register(name, version, ip, port) {
-    const key = name + version + ip + port;
+    const key = this.getKey(name, version, ip, port);
     if (!this.services[key]) {
       this.services[key] = {};
       this.services[key].timestamp = Math.floor(new Date() / 1000);
@@ -19,6 +24,14 @@ class Registry {
     }
     this.services[key].timestamp = Math.floor(new Date() / 1000);
     console.log(`Updated service ${name}, version${version} at ${ip}:${port}`);
+    return key;
+  }
+
+  unregister(name, version, ip, port) {
+    const key = this.getKey(name, version, ip, port);
+    delete this.services[key];
+    console.log(`Deleted service ${name}, version${version} at ${ip}:${port}`);
+
     return key;
   }
 }
--- workspace/microservices/registry-service/routes/index.js
+++ workspace/microservices/registry-service/routes/index.js
@@ -6,14 +6,20 @@ const registry = new Registry();
 
 const router = express.Router();
 
+function getRequestArguments(req) {
+  const { servicename, serviceversion, serviceport } = req.params;
+  let serviceip = req.ip;
+  if (serviceip.includes("::1") || serviceip.includes("::ffff:127.0.0.1")) {
+    serviceip = "127.0.0.1";
+  }
+  return { servicename, serviceversion, serviceport, serviceip };
+}
+
 router.put(
   "/register/:servicename/:serviceversion/:serviceport",
   (req, res, next) => {
-    const { servicename, serviceversion, serviceport } = req.params;
-    let serviceip = req.ip;
-    if (serviceip.includes("::1") || serviceip.includes("::ffff:127.0.0.1")) {
-      serviceip = "127.0.0.1";
-    }
+    const { servicename, serviceversion, serviceport, serviceip } =
+      getRequestArguments(req);
     const key = registry.register(
       servicename,
       serviceversion,
@@ -27,7 +33,15 @@ router.put(
 router.delete(
   "/register/:servicename/:serviceversion/:serviceport",
   (req, res, next) => {
-    return next("Not implemented");
+    const { servicename, serviceversion, serviceport, serviceip } =
+      getRequestArguments(req);
+    const key = registry.unregister(
+      servicename,
+      serviceversion,
+      serviceip,
+      serviceport
+    );
+    return res.json({ result: key });
   }
 );
 
