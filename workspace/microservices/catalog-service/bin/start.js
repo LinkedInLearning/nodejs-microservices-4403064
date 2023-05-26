c68a06e6fb0c026ed9017fac0c756bc95269111f
Daniel Khan <daniel@khan.io>
2023-05-26 16:28:41 +0200

    lifecycle

--- workspace/microservices/catalog-service/bin/start.js
+++ workspace/microservices/catalog-service/bin/start.js
@@ -11,6 +11,8 @@ const tracing = require("../lib/tracing")(
   `${config.serviceName}:${config.serviceVersion}`
 );
 
+const axios = require("axios");
+
 // Import necessary dependencies
 const http = require("http"); // HTTP server functionality
 
@@ -30,6 +32,51 @@ const server = http.createServer(app);
 server.on("listening", () => {
   const addr = server.address();
   const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+
+  const register = async () =>
+    axios
+      .put(
+        `http://127.0.0.1:3080/register/${config.serviceName}/${
+          config.serviceVersion
+        }/${server.address().port}`
+      )
+      .catch((err) => console.error(err));
+
+  const unregister = async () =>
+    axios
+      .delete(
+        `http://127.0.0.1:3080/register/${config.serviceName}/${
+          config.serviceVersion
+        }/${server.address().port}`
+      )
+      .catch((err) => console.error(err));
+
+  register();
+  const interval = setInterval(register, 10000);
+
+  const cleanup = async () => {
+    const clean = false;
+    if (!clean) {
+      clearInterval(interval);
+      await unregister();
+    }
+  };
+
+  process.on("uncaughtException", async () => {
+    await cleanup();
+    process.exit(0);
+  });
+
+  process.on("SIGTERM", async () => {
+    await cleanup();
+    process.exit(0);
+  });
+
+  process.on("SIGINT", async () => {
+    await cleanup();
+    process.exit(0);
+  });
+
   console.info(
     `${config.serviceName}:${config.serviceVersion} listening on ${bind}`
   );
