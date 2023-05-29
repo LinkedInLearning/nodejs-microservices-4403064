222ab883cdda5850daf9e68568532dedaa38df42
Daniel Khan <daniel@khan.io>
2023-05-29 11:04:10 +0200

    add service client

---
+++ workspace/shopper/server/services/ServiceClient.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+const config = require("../config");
+
+class RegistryClient {
+  static async getService(servicename) {
+    try {
+      const response = await axios.get(
+        `${config.registry.url}/find/${servicename}/${config.registry.version}`
+      );
+      if (!response.data.ip) {
+        throw new Error(
+          `Could not find ${servicename}:${config.registry.version}`
+        );
+      }
+      return response.data;
+    } catch (error) {
+      const errorMessage =
+        (error.response && error.response.data && error.data.message) ||
+        error.message;
+      throw new Error(errorMessage);
+    }
+  }
+
+  static async callService(servicename, requestOptions) {
+    const { ip, port } = await this.getService(servicename);
+    // eslint-disable-next-line no-param-reassign
+    requestOptions.url = `http://${ip}:${port}${requestOptions.url}`;
+    try {
+      const response = await axios(requestOptions);
+      return response.data;
+    } catch (error) {
+      const errorMessage =
+        (error.response && error.response.data && error.data.message) ||
+        error.message;
+      throw new Error(errorMessage);
+    }
+  }
+}
+
+module.exports = RegistryClient;
